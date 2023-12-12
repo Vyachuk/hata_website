62b4eebea8b6bf6929990cb5f75fa346ff8ce350
Vyachuk <Slavkoka045@gmail.com>
2023-12-12 19:38:57 +0200

    DELETE: fetch bot

--- app/page.js
+++ app/page.js
@@ -1,15 +1,3 @@
-const { TELEGRAM_BOT_URL } = process.env;
-
-async function fetchData() {
-  const res = await fetch(TELEGRAM_BOT_URL + "/api/wakeup");
-  const result = await res.json();
-  return result;
-}
-
 export default async function Home() {
-  setInterval(async () => {
-    await fetchData();
-  }, 1000 * 60 * 15);
-
   return <h1>Hata application</h1>;
 }
