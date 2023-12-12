c8aaeaaaa4af51ddd1d3a45eddfbbad08ae209d4
Vyachuk <Slavkoka045@gmail.com>
2023-12-12 18:10:24 +0200

    ADD: request for telegram bot

--- app/page.js
+++ app/page.js
@@ -1,11 +1,15 @@
-export default function Home() {
-  return (
-    <>
-      <header></header>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1>Hata application</h1>
-      </main>
-      <footer></footer>
-    </>
-  );
+const { TELEGRAM_BOT_URL } = process.env;
+
+async function fetchData() {
+  const res = await fetch(TELEGRAM_BOT_URL + "/api/wakeup");
+  const result = await res.json();
+  return result;
+}
+
+export default async function Home() {
+  setInterval(async () => {
+    await fetchData();
+  }, 1000 * 60 * 15);
+
+  return <h1>Hata application</h1>;
 }
